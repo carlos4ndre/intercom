451671b1eaedf09f75198f25f2a16a07d4261f9c
Carlos André <carlos.ulrich@gmail.com>
2020-01-09 10:35:10 -0500

    Add method to get all customers from repository

--- src/repository/customer.py
+++ src/repository/customer.py
@@ -3,31 +3,33 @@ import logging
 from src.constants import CUSTOMERS_FILE_PATH
 from src.utils.geo import is_point_within_range
 from src.models import Point, Customer
-from typing import List
+from typing import List, Generator
 
 logger = logging.getLogger(__name__)
 
 
 class CustomerStore:
     @staticmethod
-    def find_customers_by_distance(center: Point, distance_km: int) -> List[Customer]:
-        results = {}
-
+    def all() -> Generator:
         with open(CUSTOMERS_FILE_PATH) as ro_file:
-            logger.info("Find customers within {} km".format(distance_km))
-            all_customers = [
-                Customer(
+            for c in ijson.items(ro_file, "customers.item"):
+                yield Customer(
                     c["user_id"],
                     c["name"],
                     Point(float(c["latitude"]), float(c["longitude"])),
                 )
-                for c in ijson.items(ro_file, "customers.item")
-            ]
-            for customer in all_customers:
-                if is_point_within_range(center, customer.location, distance_km):
-                    results[customer.id] = customer
-                    logger.debug("Customer id {} is within range", customer.id)
-                else:
-                    logger.debug("Customer id {} is not within range", customer.id)
-        sorted_results = [v for (k, v) in sorted(results.items())]
+
+    @staticmethod
+    def find_customers_by_distance(center: Point, distance_km: int) -> List[Customer]:
+        results = {}
+
+        logger.info("Find customers within {} km".format(distance_km))
+        for customer in CustomerStore.all():
+            if is_point_within_range(center, customer.location, distance_km):
+                results[customer.id] = customer
+                logger.debug("Customer id {} is within range", customer.id)
+            else:
+                logger.debug("Customer id {} is not within range", customer.id)
+
+                sorted_results = [v for (k, v) in sorted(results.items())]
         return sorted_results
